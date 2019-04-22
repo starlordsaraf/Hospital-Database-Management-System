529c0870365a27207c600c4a8334d7676ee7d059
starlordsaraf <35867060+starlordsaraf@users.noreply.github.com>
2019-04-22 20:55:20 +0530

    medicine and test forms with backend

---
+++ medicines.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<?php
+require('dbinfo.php');
+?>
+<html>
+<head>
+	<title>Star Hospital | Medicines</title>
+	<link rel="stylesheet" type="text/css" href="register.css">
+	<link rel="icon" type="image" href="logo.jpg">
+</head>
+<body background="tag2.jpg">
+	<div class="main">&nbsp;&nbsp;&nbsp;STAR HOSPITAL
+		<span>
+			<a href="reg_pat.php" class="navi">Register Patient</a>&nbsp;|&nbsp;
+			<a href="reg_emp.php" class="navi">Register Employee</a>&nbsp;|&nbsp;
+			<a href="index.html" class="navi">Home</a>&nbsp;|&nbsp;
+			<a href="tests.php" class="navi">Tests</a>&nbsp;|&nbsp;
+			<a href="checkdetails.html" class="navi">Check Details</a>&nbsp;|&nbsp;
+			<a href="contactus.html" class="navi">Contact Us </a>&nbsp;
+		</span>
+	</div>
+
+	<div align="center">
+		<h1>Medicine Selection</h1>
+		<form id="emp_form" method="POST">
+			<label>
+				Patient ID:
+				<input type="text" name="pid">
+			</label><br><br>
+			
+			<label>
+				Medicine Name:
+				<select name="med">
+					<option>Sinarest</option>
+					<option>Citzin</option>
+					<option>Asprin</option>
+					<option>Wararin</option>
+					<option>Insulin-A</option>
+					<option>Glynase</option>
+					<option>Paracetamol</option>
+				</select>
+			</label><br><br>
+
+			<label>
+				Quantity:
+				<input type="number" name="qty">
+			</label><br><br>
+
+
+			<label>
+				Purchase Date:
+				<input type="text" name="mdate" placeholder="mm-dd-yyyy">
+			</label><br><br>
+
+			<input type="submit" name="submit">
+			
+		</form>
+	</div>	
+
+	<?php
+	if(isset($_POST['submit']))	
+	{
+		$pid="$_POST[pid]";
+		$mdate="$_POST[mdate]";
+		$medicine="$_POST[med]";
+		$qty="$_POST[qty]";
+
+		$check_query="select pid from patient";
+		$check=pg_query($db,$check_query);
+		$check_result=pg_fetch_all_columns($check);
+
+		if(in_array("$pid", $check_result)===FALSE)
+			echo "Enter Valid Patient ID!!";
+
+		else
+		{
+			$medid_query="SELECT med_id FROM medicine WHERE med_name='$medicine'";
+			$m=pg_query($db,$medid_query);
+			$medid=pg_fetch_assoc($m);
+
+			$query="INSERT INTO takes VALUES('$medid[med_id]','$pid','$mdate','$qty')";
+			$result=pg_query($db,$query);
+
+			echo "New Medicine Purchased";
+		}
+
+	}
+
+	?>
+
+</body>
+</html>
---
+++ tests.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<?php
+require('dbinfo.php');
+?>
+
+<html>
+<head>
+	<title>Star Hospital | Tests</title>
+	<link rel="stylesheet" type="text/css" href="register.css">
+	<link rel="icon" type="image" href="logo.jpg">
+</head>
+
+<body background="tag2.jpg">
+	<div class="main">&nbsp;&nbsp;&nbsp;STAR HOSPITAL
+		<span>
+			<a href="reg_pat.php" class="navi">Register Patient</a>&nbsp;|&nbsp;
+			<a href="reg_emp.php" class="navi">Register Employee</a>&nbsp;|&nbsp;
+			<a href="index.html" class="navi">Home</a>&nbsp;|&nbsp;
+			<a href="medicines.php" class="navi">Medicines</a>&nbsp;|&nbsp;
+			<a href="checkdetails.html" class="navi">Check Details</a>&nbsp;|&nbsp;
+			<a href="contactus.html" class="navi">Contact Us </a>&nbsp;
+		</span>
+	</div>
+	<div align="center">
+		<h1>Test Selection</h1>
+		<form id="emp_form" method="POST">
+			<label>
+				Patient ID:
+				<input type="text" name="pid">
+			</label><br><br>
+			
+			<label>
+				Test Name:
+				<select name="test">
+					<option>Troponin Test</option>
+					<option>CK-MB Test</option>
+					<option>Serum Myoglobin Test</option>
+					<option>Random Blood Sugar Test</option>
+					<option>Fasting Blood Sugar Test</option>
+					<option>Oral Glucose Tolerence Test</option>
+				</select>
+			</label><br><br>
+
+			<label>
+				Test Date:
+				<input type="text" name="tdate" placeholder="mm-dd-yyyy">
+			</label><br><br>
+
+			<input type="submit" name="submit">
+			
+		</form>
+	</div>	
+
+	<?php
+	if(isset($_POST['submit']))	
+	{
+		$pid="$_POST[pid]";
+		$tdate="$_POST[tdate]";
+		$test="$_POST[test]";
+
+		$check_query="select pid from patient";
+		$check=pg_query($db,$check_query);
+		$check_result=pg_fetch_all_columns($check);
+
+		if(in_array("$pid", $check_result)===FALSE)
+			echo "Enter Valid Patient ID!!";
+
+		else
+		{
+			$testid_query="SELECT test_id FROM test WHERE test_name='$test'";
+			$t=pg_query($db,$testid_query);
+			$testid=pg_fetch_assoc($t);
+
+			$query="INSERT INTO has VALUES('$testid[test_id]','$pid','$tdate')";
+			$result=pg_query($db,$query);
+
+			echo "New Test Record Added";
+		}
+
+	}
+
+	?>
+
+
+</body>
+</html>
